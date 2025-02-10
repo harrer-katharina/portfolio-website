55fb862e58aee9e626201d8699206955b387edb5
harrer-katharina <HarrerKathi@gmail.com>
2025-02-10 10:05:43 +0100

    fix: fix auto theme switch

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -10,6 +10,8 @@ import Contact from "@/components/contact";
 export default function Home() {
   return (
     <main>
+      <div className="bg-[#fbe2e3] dark:bg-gray-800 fixed top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+      <div className="bg-[#ffd2e9] dark:bg-gray-800 fixed top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
       <div className="flex flex-col items-center px-4">
         <Intro />
         <ParallaxSkills />
