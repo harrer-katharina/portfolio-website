5088782b3f7ccaf016e185e54ce024038f96e59a
harrer-katharina <HarrerKathi@gmail.com>
2025-07-28 12:44:22 +0200

    chore: add sitemap and robots.txt generation with next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://katharina-harrer.de',
+  generateRobotsTxt: true,
+  changefreq: 'monthly',
+  priority: 0.7,
+  sitemapSize: 5000,
+};
